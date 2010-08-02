a10ce9dd0177437c5eb75b1164aa86786eb77f86
clican <clican@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2010-08-02 09:21:12 +0000

    define xml parser for state parsing based on the spring xml extension

---
+++ trunk/modules/pluto-module-fsm/src/main/java/com/clican/pluto/fsm/spring/handler/FsmNamespaceHandler.java
@@ -0,0 +1,27 @@
+/**
+ * The Clican-Pluto software suit is Copyright 2009, Clican Company
+ * and individual contributors, and is licensed under the GNU LGPL.
+ *
+ * @author wei.zhang
+ *
+ */
+package com.clican.pluto.fsm.spring.handler;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * This handler is used to extend the spring xml to support fsm namespace.
+ *
+ * @author wei.zhang
+ *
+ */
+public class FsmNamespaceHandler extends NamespaceHandlerSupport {
+
+	public void init() {
+
+	}
+
+}
+
+
+//$Id$
