45eb90441746a70a828379b6a606ff38b998312f
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2012-06-04 14:07:02 +0000

    2

--- ccdream/ccdream/PositionUtil.m
+++ ccdream/ccdream/PositionUtil.m
@@ -12,18 +12,18 @@
 @implementation PositionUtil
 
 +(CCArray*) calcMoveOrbitarrayFromPosition:(Position*) charPosi movement:(int) movement mobility:(Mobility*) mobility mapTypeMetrix:(NSDictionary*) mapTypeMetrix maxPosition:(Position*) maxPosition{
-    NSSet* moveOrbitSet = [NSSet set];
-    NSDictionary* processedPosiMap = [NSDictionary dictionary];
+    NSMutableDictionary* moveOrbitSet = [[[NSMutableDictionary alloc] init] autorelease];
+    NSMutableDictionary* processedPosiMap = [[[NSMutableDictionary alloc] init] autorelease];
     
     MoveOrbit* current = [MoveOrbit initWithPrevious:nil Position:charPosi];
     [PositionUtil moveForProcessedposiMap: processedPosiMap moveOrbitSet: moveOrbitSet previousMoveOrbit: current movement: movement mobility:mobility mapTypeMetrix: mapTypeMetrix maxPosition: maxPosition xoffset:0 yoffset:0];
-    NSArray* sortedArray = [[moveOrbitSet allObjects] sortedArrayUsingComparator:[MoveOrbit comparator]];
+    NSArray* sortedArray = [[moveOrbitSet allValues] sortedArrayUsingComparator:[MoveOrbit comparator]];
     CCArray* result = [CCArray arrayWithNSArray:sortedArray];
     return result;   
 }
 
 
-+(void) moveForProcessedposiMap:(NSDictionary*) processedPosiMap moveOrbitSet:(NSSet*) moveOrbitSet previousMoveOrbit:(MoveOrbit*) previousMoveOrbit movement:(int) movement mobility:(Mobility*)mobility mapTypeMetrix:(NSDictionary*) mapTypeMetrix maxPosition:(Position*) maxPosition xoffset:(int)xoffset yoffset:(int)yoffset{
++(void) moveForProcessedposiMap:(NSMutableDictionary*) processedPosiMap moveOrbitSet:(NSMutableDictionary*) moveOrbitSet previousMoveOrbit:(MoveOrbit*) previousMoveOrbit movement:(int) movement mobility:(Mobility*)mobility mapTypeMetrix:(NSDictionary*) mapTypeMetrix maxPosition:(Position*) maxPosition xoffset:(int)xoffset yoffset:(int)yoffset{
     Position* currentPosi = [Position initWithX:previousMoveOrbit.position.x+xoffset Y:previousMoveOrbit.position.y+yoffset];
     NSNumber* remainingMovement = (NSNumber*)[processedPosiMap objectForKey:[currentPosi description]];
     if(remainingMovement!=nil&&[remainingMovement intValue]>=movement){
@@ -35,7 +35,7 @@
     int x = currentPosi.x;
     int y = currentPosi.y;
     
-    int mapType = [(NSNumber*)[mapTypeMetrix objectForKey:currentPosi] intValue];
+    int mapType = [(NSNumber*)[mapTypeMetrix objectForKey:[currentPosi description]] intValue];
     int moveCost = 0;
     
     //如果不是起始点
