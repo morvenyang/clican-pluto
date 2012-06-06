4c981b70db3a438f273812ae1757b8c377f562d7
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2012-06-06 06:31:57 +0000

    终于写完苦力代码了

--- trunk/ccdream/ccdream/Session.h
+++ trunk/ccdream/ccdream/Session.h
@@ -10,7 +10,30 @@
 #import "cocos2d.h"
 
 @interface Session : NSObject {
+    int _sessionId;
+    NSString* _title;
+    NSString* _name;
+    NSString* _type;
+    NSString* _sponsor;
+    NSDate* _startTime;
+    NSDate* _lastUpdateTime;
+    NSDate* _endTime;
+    NSArray* _states;
+    NSArray* _variables;
+    NSString* _status;
     
 }
+
+@property (nonatomic,assign) int sessionId;
+@property (nonatomic,retain) NSString* title;
+@property (nonatomic,retain) NSString* name;
+@property (nonatomic,retain) NSString* type;
+@property (nonatomic,retain) NSString* sponsor;
+@property (nonatomic,retain) NSDate* startTime;
+@property (nonatomic,retain) NSDate* lastUpdateTime;
+@property (nonatomic,retain) NSDate* endTime;
+@property (nonatomic,retain) NSArray* states;
+@property (nonatomic,retain) NSArray* variables;
+@property (nonatomic,retain) NSString* status;
 
 @end
--- trunk/ccdream/ccdream/Session.m
+++ trunk/ccdream/ccdream/Session.m
@@ -11,4 +11,40 @@
 
 @implementation Session
 
+@synthesize sessionId = _sessionId;
+@synthesize title = _title;
+@synthesize name = _name;
+@synthesize type = _type;
+@synthesize sponsor = _sponsor;
+@synthesize startTime = _startTime;
+@synthesize lastUpdateTime = _lastUpdateTime;
+@synthesize endTime = _endTime;
+@synthesize states = _states;
+@synthesize variables = _variables;
+@synthesize status = _status;
+
+- (void)dealloc {
+    [_name release];
+    _name = nil;
+    [_title release];
+    _title = nil;
+    [_type release];
+    _type = nil;
+    [_sponsor release];
+    _sponsor = nil;
+    [_startTime release];
+    _startTime = nil;
+    [_lastUpdateTime release];
+    _lastUpdateTime = nil;
+    [_endTime release];
+    _endTime = nil;
+    [_states release];
+    _states = nil;
+    [_variables release];
+    _variables = nil;
+    [_status release];
+    _status = nil;
+    [super dealloc];
+}
+
 @end
