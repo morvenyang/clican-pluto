2222ed3dc494d9be150d5e026b171d495abd1ce5
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2012-06-07 03:31:32 +0000

    1

--- ccdream/ccdream/EngineContext.m
+++ ccdream/ccdream/EngineContext.m
@@ -13,41 +13,11 @@
 @synthesize sessionMap = _sessionMap;
 
 -(void) loadConfig:(NSString*) name {
-    NSError* error;
-    NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:@"xml"];
-    NSString *fileContent = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
-    GDataXMLDocument *document = [[GDataXMLDocument alloc] initWithXMLString:fileContent options:1 error:&error];
-    GDataXMLElement *rootElement = [document rootElement];
-    CCLOG(@"******rootElement******\n%@",rootElement);
-    
+        
     
 }
 
--(void) parseDocument:(GDataXMLElement*) element stateMap:(NSMutableDictionary*) stateMap, istate:(IState*) istate {    
 
-    for(int i=0;i<element.childCount;i++){
-        GDataXMLNode* node = [element childAtIndex:i];
-        if([node isKindOfClass:GDataXMLElement]){
-            GDataXMLElement* e = (GDataXMLElement*)node;
-            NSString* elementName = [e name];
-            BOOL stateElement = YES;
-            if([elementName isEqualToString:@"start"]){
-                istate = [[[StartState alloc] init] autorelase];
-            }else if([elementName isEqualToString:@"end"]){
-                istate = [[[EndState alloc] init] autorelase];
-            }else if([elementName isEqualToString:@"state"]){
-                NSString* clsssName = [[e attributeForName:@"class"] stringValue];
-                if(className!=nil&&[className length]>){
-                    
-                }
-            }else if([elementName isEqualToString:@"task"]){
-                
-            }
-        }else{
-            
-        }
-    }
-}
 
 
 -(Session*) newSession:(NSString*) name forSponsor:(NSString*) sponsor{
