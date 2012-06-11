78aa533d244542f876804e91b6cd956937a4f76a
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2012-06-11 10:59:56 +0000

    add weapon

---
+++ trunk/ccdream/ccdream/Weapon.m
@@ -0,0 +1,14 @@
+//
+//  Weapon.m
+//  ccdream
+//
+//  Created by wei zhang on 12-6-10.
+//  Copyright 2012年 Clican. All rights reserved.
+//
+
+#import "Weapon.h"
+
+
+@implementation Weapon
+
+@end
