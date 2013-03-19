3e840a4296b81c1b8badbdbb7b0e48c943c8f2e0
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2013-03-19 09:38:03 +0000

    support background mode

--- trunk/ios/appletvserver/appletvserver/AppDelegate.m
+++ trunk/ios/appletvserver/appletvserver/AppDelegate.m
@@ -150,8 +150,7 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [self continuousServer];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -175,4 +174,32 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void) continuousServer
+{
+    UIApplication*    app = [UIApplication sharedApplication];
+    
+    bgTask = [app beginBackgroundTaskWithExpirationHandler:^{
+        // Clean up any unfinished task business by marking where you.
+        // stopped or ending the task outright.
+        [app endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
+    }];
+    
+    // Start the long-running task and return immediately.
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        audioPlayer = [[MTAudioPlayer alloc]init];
+        [audioPlayer playBackgroundAudio];
+        
+        // Must comment out or else will stop server
+        // [app endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
+    });
+}
+
+- (void) tempServer
+{
+    audioPlayer = [[MTAudioPlayer alloc]init];
+    [audioPlayer playBackgroundAudio];
+}
 @end
