653112935628ba5a91aead99a1c676434e0ffbc6
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2013-03-25 09:15:50 +0000

    add touch xml

---
+++ trunk/ios/appletvserver/appletvserver/XmlViewController.m
@@ -0,0 +1,51 @@
+//
+//  XmlViewController.m
+//  appletvserver
+//
+//  Created by zhang wei on 13-3-25.
+//
+//
+
+#import "XmlViewController.h"
+
+
+
+@implementation XmlViewController
+
+@synthesize xml = _xml;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+-(id) initWithXml:(NSString*) xml{
+    self = [super init];
+    if(self){
+        self.xml = xml;
+    }
+    return self;
+}
+- (void)dealloc
+{
+    
+    TT_RELEASE_SAFELY(_xml);
+    [super dealloc];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
