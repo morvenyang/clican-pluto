4c6a162cd7898727208d1221ef38f4b66fdeb707
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2013-03-25 12:43:37 +0000

    implement channel page by ejs

--- ios/appletvserver/appletvserver/AjaxCallbackRequest.m
+++ ios/appletvserver/appletvserver/AjaxCallbackRequest.m
@@ -20,28 +20,4 @@
     req.ctx = ctx;
     return req;
 }
-
-#pragma mark -
-#pragma mark TTURLRequestDelegate
-
-- (void)requestDidFinishLoad:(TTURLRequest*)request
-{
-    @try {
-        TTURLDataResponse* response = request.response;
-        
-        NSString* content = [[[NSString alloc] initWithData:[response data] encoding:NSUTF8StringEncoding] autorelease];
-        NSLog(@"content:%@" ,content);
-        JSValueRef args[1];
-        args[0] = JSValueMakeString(self.ctx,JSStringCreateWithUTF8CString([content UTF8String]));
-        JSObjectCallAsFunction(self.ctx,self.callback,NULL,1,args,NULL);
-    }
-    @catch (NSException *exception) {
-        TTAlert([NSString stringWithFormat:@"错误:%@",[exception name]]);
-        
-    }
-}
-
-- (void)request:(TTURLRequest*)request didFailLoadWithError:(NSError*)error {
-    NSLog(@"request:%@ didFailLoadWithError:%@", request, error);
-}
 @end
