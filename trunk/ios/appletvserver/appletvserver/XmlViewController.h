db6cc510c740019e10028b93bf1fb2860e275073
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2013-03-26 07:32:51 +0000

    implement index page

--- trunk/ios/appletvserver/appletvserver/XmlViewController.h
+++ trunk/ios/appletvserver/appletvserver/XmlViewController.h
@@ -11,9 +11,14 @@
 @interface XmlViewController : TTViewController<UITableViewDelegate>{
     NSString* _xml;
     NSString* _type;
+    BOOL _append;
+    NSMutableArray* _videos;
 }
 @property (nonatomic, copy) NSString* xml;
 @property (nonatomic, copy) NSString* type;
+@property (nonatomic, assign) BOOL append;
+@property (nonatomic, retain) NSMutableArray* videos;
 
 -(id) initWithXml:(NSString*) xml;
+-(void) appendXml:(NSString*) xml;
 @end
