14f3dcf57cbaf7ba7c0fd1057d5207fe21d7a76b
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2014-01-15 03:48:59 +0000

    change some demo data

--- ikidstv/quiz/src/main/java/com/ikidstv/quiz/service/impl/ContentServiceImpl.java
+++ ikidstv/quiz/src/main/java/com/ikidstv/quiz/service/impl/ContentServiceImpl.java
@@ -21,25 +21,29 @@ public class ContentServiceImpl implements ContentService {
 		root.setName("iKidsTV");
 		root.setContentId("1");
 
-		ContentTree tree1 = new ContentTree();
-		tree1.setName("TV");
-		tree1.setContentId("2");
-		root.getSubTree().add(tree1);
-
-		ContentTree tree2 = new ContentTree();
-		tree2.setName("Cartoon");
-		tree2.setContentId("3");
-		root.getSubTree().add(tree2);
-
 		ContentTree season = new ContentTree();
-		season.setName("Mickey garden");
+		season.setName("Mickey Mouse Clubhouse S1");
 		season.setContentId("4");
 		season.setSeasonId("4");
 		season.setSeasonNode(true);
-		tree2.getSubTree().add(season);
+		root.getSubTree().add(season);
+		
+		ContentTree season2 = new ContentTree();
+		season.setName("Jake and the Never Land Pirates");
+		season.setContentId("2");
+		season.setSeasonId("2");
+		season.setSeasonNode(true);
+		root.getSubTree().add(season2);
+		
+		ContentTree season3 = new ContentTree();
+		season.setName("Octonauts");
+		season.setContentId("3");
+		season.setSeasonId("3");
+		season.setSeasonNode(true);
+		root.getSubTree().add(season3);
 
 		ContentTree e1 = new ContentTree();
-		e1.setName("Mickey garden#1");
+		e1.setName("Mickey Mouse Clubhouse S1#1");
 		e1.setContentId("5");
 		e1.setSeasonId("4");
 		e1.setEpisonId("5");
@@ -48,7 +52,7 @@ public class ContentServiceImpl implements ContentService {
 		season.getSubTree().add(e1);
 
 		ContentTree e2 = new ContentTree();
-		e2.setName("Mickey garden#2");
+		e2.setName("Mickey Mouse Clubhouse S1#2");
 		e2.setContentId("6");
 		e2.setSeasonId("4");
 		e2.setEpisonId("6");
@@ -57,7 +61,7 @@ public class ContentServiceImpl implements ContentService {
 		season.getSubTree().add(e2);
 
 		ContentTree e3 = new ContentTree();
-		e3.setName("Mickey garden#3");
+		e3.setName("Mickey Mouse Clubhouse S1#3");
 		e3.setContentId("7");
 		e3.setSeasonId("4");
 		e3.setEpisonId("7");
