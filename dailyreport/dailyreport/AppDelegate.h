1d4dbefa35718c9288c6eee89ec2bc7790b1cab6
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2014-05-17 03:11:46 +0000

    import

---
+++ dailyreport/dailyreport/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dailyreport
+//
+//  Created by zhang wei on 14-5-16.
+//  Copyright (c) 2014年 Peace Bird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
