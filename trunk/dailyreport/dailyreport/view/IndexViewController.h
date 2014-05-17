05803325aa0ec4c4b87f9bc9eef59b93862cbd4f
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2014-05-17 03:11:46 +0000

    import

---
+++ trunk/dailyreport/dailyreport/view/IndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  IndexViewController.h
+//  dailyreport
+//
+//  Created by zhang wei on 14-5-17.
+//  Copyright (c) 2014年 Peace Bird. All rights reserved.
+//
+
+#import <Three20UI/Three20UI.h>
+
+@interface IndexViewController : TTTableViewController
+
+@end
