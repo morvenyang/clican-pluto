0de5aa3483edfd0f0520e3bbd1e1bf68c71988c2
clican@gmail.com <clican@gmail.com@084c1b98-d6c6-0bf6-17f4-9fa91a389573>
2014-11-11 08:23:53 +0000

    123

--- trunk/HCMas/HCMas/view/ChartViewController.m
+++ trunk/HCMas/HCMas/view/ChartViewController.m
@@ -65,7 +65,7 @@
         
         //对navigationController.view 进行强制旋转
         self.navigationController.view.transform = CGAffineTransformMakeRotation(arch);
-        self.navigationController.view.bounds = CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        self.navigationController.view.bounds = CGRectMake(0,0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     
 }
@@ -126,11 +126,13 @@
     }
     NSLog(@"%f,%f",SCREEN_WIDTH,SCREEN_HEIGHT);
     long height = SCREEN_WIDTH;
-    UIScrollView* scrollView = [[[UIScrollView alloc] initWithFrame:CGRectMake(0,0,SCREEN_HEIGHT,height)] autorelease];
+    long yOffset = 44;
+    UIScrollView* scrollView = [[[UIScrollView alloc] initWithFrame:CGRectMake(0,yOffset,SCREEN_HEIGHT,height-yOffset)] autorelease];
+    scrollView.bounds = CGRectInset(CGRectMake(0,0,SCREEN_HEIGHT,height-yOffset), 0, -yOffset);
     if(self.webPieChartView){
         [self.webPieChartView removeFromSuperview];
     }
-    self.webPieChartView = [[[UIWebView alloc] initWithFrame:CGRectMake(0,0,width/2,height)] autorelease];
+    self.webPieChartView = [[[UIWebView alloc] initWithFrame:CGRectMake(0,0,width/2,height-yOffset)] autorelease];
     
     self.webPieChartView.scrollView.scrollEnabled=NO;
     NSString* dataProvider = [[[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:kpis options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding] autorelease];
@@ -140,7 +142,7 @@
     
     html=[html stringByReplacingOccurrencesOfString:@"$dataProvider" withString:dataProvider];
     html=[html stringByReplacingOccurrencesOfString:@"$width" withString:[NSString stringWithFormat:@"%li",width]];
-    html=[html stringByReplacingOccurrencesOfString:@"$height" withString:[NSString stringWithFormat:@"%li",height*2]];
+    html=[html stringByReplacingOccurrencesOfString:@"$height" withString:[NSString stringWithFormat:@"%li",(height-yOffset)*2]];
     html=[html stringByReplacingOccurrencesOfString:@"$unit" withString:[self getUnitForKpiType:self.kpiType]];
 
     NSLog(@"%@",html);
@@ -149,9 +151,15 @@
     
     [scrollView addSubview:self.webPieChartView];
     [self.view addSubview:scrollView];
-    scrollView.contentSize = CGSizeMake(width/2,height);
-    self.webPieChartView.scalesPageToFit=YES;
+    scrollView.contentSize = CGSizeMake(width/2,height-yOffset);
+
+    
+    
+    self.webPieChartView.scalesPageToFit= YES;
+    
     self.webPieChartView.userInteractionEnabled =YES;
+    NSLog(@"%f,%f",self.webPieChartView.frame.size.width,self.webPieChartView.frame.size.height);
+     NSLog(@"%f,%f",scrollView.frame.size.width,scrollView.frame.size.height);
 }
 - (void)loadKpiHistoryFailed:(NSError*) error message:(NSString*) message{
 
